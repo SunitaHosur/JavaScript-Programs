d745cad7079c45ce5ebe7391bbc6fd140ebb7d9f
SunitaHosur <sunitahosur81@gmail.com>
2020-12-15 22:14:37 +0530

    Palindome

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+const str = "kanak";
+const isPalindrome=(str)=>{
+        for(var i=0; i<=str.length; i++){
+    if(str[i]!==str[str.length-1-i]){
+        return false;
+    }
+}
+return true;
+}
+
+console.log(isPalindrome(str));
